abaaabdee3841fbd5b6a0f9458c61cd08f37bb69
EvgeniyPosohin <mad.house@bk.ru>
2025-08-05 15:09:57 +0500

    adapting the appearance of models

--- PersonBlog/Blog/admin.py
+++ PersonBlog/Blog/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Post, Blog, Comment
 
-# Register your models here.
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ['author', 'title', 'created_at', 'updated_at']
+    list_filter = ['author', 'title', 'created_at', 'updated_at']
+    search_fields = ['title', 'description']
+    raw_id_fields = ['author']
+    ordering = ['created_at', 'updated_at']
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at', 'updated_at']
+    list_filter = ['title', 'created_at', 'updated_at']
+    search_fields = ['title', 'content']
+    ordering = ['created_at', 'updated_at']
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['author', 'content', 'created_at']
+    list_filter = ['author', 'content', 'created_at']
+    search_fields = ['author', 'created_at']
+    raw_id_fields = ['author']
+    ordering = ['created_at']
